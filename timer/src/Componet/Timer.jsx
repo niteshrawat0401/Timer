3b8433443a94b6319c5a5ceb1b2b672101b87ed9
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-27 01:44:11 +0530

    format page

--- timer/src/Componet/Timer.jsx
+++ timer/src/Componet/Timer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./Timer.css"
+import "./Timer.css";
 export const Timer = () => {
   const [minute, setMinute] = useState(0);
   const [seconds, setSeconds] = useState(0);
@@ -31,14 +31,14 @@ export const Timer = () => {
     <div>
       <h1>Stop watch</h1>
       <div className="outerDiv">
-     <div className="innerDiv">
-        {minute < 10 ? "0" + minute : minute} :{" "}
-        {seconds < 10 ? "0" + seconds : seconds}
+        <div className="innerDiv">
+          {minute < 10 ? "0" + minute : minute} :{" "}
+          {seconds < 10 ? "0" + seconds : seconds}
         </div>
       </div>
       <div className="btnDiv">
-      <button onClick={handleStop}>Stop</button>
-      <button onClick={handleRestart}>Restart</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleRestart}>Restart</button>
       </div>
     </div>
   );
