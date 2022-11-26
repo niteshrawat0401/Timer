6b806cb6c44f027a32bf8796412d7d253636964c
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-11-27 01:18:38 +0530

    stop restart function

--- timer/src/Componet/Timer.jsx
+++ timer/src/Componet/Timer.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from "react"
-export const Timer=()=>{
-    const [minute, setMinute]= useState(0);
-    const [seconds, setSeconds]= useState(0);
+import React, { useEffect, useState } from "react";
+export const Timer = () => {
+  const [minute, setMinute] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
-    
-    function handleChange(){
-        setInterval(()=>{
-            setMinute(seconds+1)
-            if(seconds == 59){
-                setMinute(minute + 1);
-                setSeconds(0)
-            }
-        })
-    }
-    
+  var timer;
 
-    return(
-        <div>
-            <h1>Stop watch</h1>
-            <p>{minute} : {seconds}</p>
-            <button onClick={handleChange}>Start</button>
-        </div>
-    )
-}
+  useEffect(() => {
+    
+      timer = setInterval(() => {
+        setSeconds(seconds + 1);
+        if (seconds == 59) {
+          setMinute(minute + 1);
+          setSeconds(0);
+        }
+      },1000);
+      return () => {
+      clearInterval(timer);
+    };
+  });
+
+  function handleStop() {
+    clearInterval(timer);
+  }
+  function handleRestart() {
+    setMinute(0);
+    setSeconds(0);
+  }
+
+  return (
+    <div>
+      <h1>Stop watch</h1>
+      <p>
+        {minute} : {seconds}
+      </p>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleRestart}>Restart</button>
+    </div>
+  );
+};
